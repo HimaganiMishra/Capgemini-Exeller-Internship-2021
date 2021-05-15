628516932fcaf7f4b4c120d065bd0dbfd75364a3
Himagani Mishra <himaganim@gmail.com>
2021-05-15 18:31:21 +0530

    Postman Demo

---
+++ SpringBoot/MyShoppingCart/src/main/java/com/capg/msc/myshoppingcart/dao/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.capg.msc.myshoppingcart.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+	import com.capg.msc.myshoppingcart.beans.Product;
+
+	public interface ProductRepository extends JpaRepository<Product, Integer> {
+		
+		/*
+		 * findAll
+		 * save
+		 * update
+		 * read
+		 * delete
+		 * all added automatically
+		 * */
+
+	
+
+}
---
+++ SpringBoot/MyShoppingCart/src/main/java/com/capg/msc/myshoppingcart/service/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+
+	package com.capg.msc.myshoppingcart.service;
+
+	import java.util.List;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Service;
+	import org.springframework.transaction.annotation.Transactional;
+
+	import com.capg.msc.myshoppingcart.beans.Product;
+	import com.capg.msc.myshoppingcart.dao.ProductRepository;
+
+	@Service
+	public class ProductServiceImpl implements ProductService {
+		
+		@Autowired
+		ProductRepository repo;
+
+		@Override
+		public List<Product> getAllProducts() {
+			
+			return repo.findAll();
+		}
+		
+		
+		
+		@Override
+		@Transactional
+		public Product saveProduct(Product p) {
+			
+			repo.save(p);
+			return null;
+			
+			
+		}
+
+	}
+
