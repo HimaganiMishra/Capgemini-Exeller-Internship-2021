421dd016862809a73ad172de2a37902fd52eb4cb
Himagani Mishra <himaganim@gmail.com>
2021-05-15 18:03:54 +0530

    LabBook

---
+++ Lab/src/lab5Excercise1/InvalidInputException.java
@@ -0,0 +1,10 @@
+package lab5Excercise1;
+
+class InvalidInputException extends Exception {
+	
+	InvalidInputException(String exceptionText)
+	{
+		super(exceptionText);
+	}
+
+}
---
+++ Lab/src/lab5Excercise3/EmployeeException.java
@@ -0,0 +1,9 @@
+package lab5Excercise3;
+
+class EmployeeException extends Exception {
+	 
+	EmployeeException(String exceptionText)
+		{
+			super(exceptionText);
+		}
+}
